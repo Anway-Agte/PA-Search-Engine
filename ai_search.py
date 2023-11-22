fa4696c371fda673926f98cae8c37ad60f8ad42e
ian-Liaozy <zl3057@nyu.edu>
2023-11-22 11:10:01 -0500

    fast api and txtai app

--- ai_search.py
+++ ai_search.py
@@ -1,7 +1,7 @@
 from txtai.embeddings import Embeddings
 # import txtai as t
 import json
-import os
+# import os
 import urllib3
 
 def ai_search(query_text):
@@ -10,18 +10,28 @@ def ai_search(query_text):
     })
 
     txtai_data = []
-    i = 0
 
-    file_list = os.listdir("PA_LENR/Clean data/")
+    # file_list = os.listdir("PA_LENR/Clean data/")
     # http = urllib3.PoolManager()
     # url = "https://github.com/Anway-Agte/PA-Search-Engine/tree/ai-search/PA_LENR/Clean%20data"
     # resp = urllib3.request("GET", url)
     # print(resp.status, resp.data)
 
-    for idx, filename in enumerate(file_list):
-        with open("PA_LENR/Clean data/"+filename, 'r') as f:
-            text = filename + "\n" + f.read()
-            txtai_data.append((idx, text, None))
+    # for idx, filename in enumerate(file_list):
+    #     with open("PA_LENR/Clean data/"+filename, 'r') as f:
+    #         text = filename + "\n" + f.read()
+    #         txtai_data.append((idx, text, None))
+    txtai_idx = 0
+    for i in range(3547):
+        url = "https://github.com/Anway-Agte/PA-Search-Engine/tree/ai-search/PA_LENR/Clean%20data/Copy%20of%20"+str(i)+".txt"
+        try:
+            resp = urllib3.request("GET", url)
+            print(resp.status, resp.data)
+            txtai_data.append((txtai_idx, resp.data, None))
+            txtai_idx += 1
+        except:
+            pass
+
 
 
     embeddings.index(txtai_data)
